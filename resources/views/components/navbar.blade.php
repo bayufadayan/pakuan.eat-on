00b8053b67629ec6435459d50ed892d18477c126
bayufadayan <bayufadayan@gmail.com>
2023-05-22 05:20:20 +0700

    add many folder and file for front-end

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,48 @@
+<nav>
+    <ul>
+        <li>
+            <a href="#">
+                <i class="fa-solid fa-user"></i>
+                <span class="nav-item">account</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <span  class="nav-item">Search</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class="fa-solid fa-house"></i>
+                <span  class="nav-item">Home</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class="fa-solid fa-heart"></i>
+                <span class="nav-item">Favorite</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class="fa-solid fa-bag-shopping"></i>
+                <span class="nav-item">MyBag</span>
+            </a>
+        </li>
+       <div class="bottom-menu">
+            <li>
+                <a href="#" class="bellow">
+                        <i class="fa-solid fa-circle-info"></i>
+                        <span class="nav-item">About us</span>
+                </a>
+                </li>
+            <li>
+                <a href="#" class="bellow">
+                        <i class="fa-solid fa-right-from-bracket"></i>
+                        <span class="nav-item">Log out</span>
+                </a>
+            </li>
+       </div>
+    </ul>
+</nav>
---
+++ resources/views/favorite.blade.php
@@ -0,0 +1,1 @@
+<h1>Halaman favorite</h1>
